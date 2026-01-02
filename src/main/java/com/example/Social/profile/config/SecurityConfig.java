6bbb86d7379c0e256f6a69ffca24da91391b5373
deepakSingh023 <dsinghrana71@gmail.com>
2026-01-02 14:24:43 +0530

    fixes in security

--- src/main/java/com/example/Social/profile/config/SecurityConfig.java
+++ src/main/java/com/example/Social/profile/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/profiles/**").permitAll()
                         .anyRequest().permitAll()
-                )
+                );
 
 
         return http.build();
